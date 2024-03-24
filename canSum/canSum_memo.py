caed4238ad133df7b34e96e2b9b6611d0f859e15
azh-ba <anhbanguyenquoc@gmail.com>
2024-03-24 23:13:30 +0700

    Update canSum_memo.py

--- canSum/canSum_memo.py
+++ canSum/canSum_memo.py
@@ -1,55 +1,58 @@
 import os
 
-def canSum(targetSum, numbers, memo={}):
+def canSum(targetSum, numbers, memo={}, level=0):
+    """Return True if the targetSum can be computed using numbers.
+    False if otherwise.
+    targetSum       int
+    numbers         list
+    memo            dict        {key:value} = {7:False}
+    level           int         note the current level of the recursive tree.
+                                increase whenever travel down from the root.
+                                decrease whenever travel up to the root.
+                                if travel back to the root, delete memo.
+    """
+    # increase level whenever travel further down the recursive tree
+    level += 1
+
+    # base case: result is saved in memo --> decrease level, return memo
     if targetSum in memo:
+        level -= 1
         return memo[targetSum]
-    if targetSum == 0:
+    
+    # base case: targetSum = 0 --> decrease level, return True
+    if targetSum == 0:  
+        level -= 1
         return True
+    
+    # base case: targetSum < 0 --> decrease level, return False
     if targetSum < 0:
+        level -= 1
         return False
+    
+    # branching
     for num in numbers:
         remainder = targetSum - num
-        if canSum(remainder, numbers) == True:
+        # case: if remainder is reachable --> save result to memo, decrease level, then return True
+        if canSum(remainder, numbers, memo, level) == True:
             memo[targetSum] = True
+            level -= 1
+            # if reached the root (level == 0) --> delete the memo for further usage
+            if level == 0:
+                memo.clear()
             return True
-    memo[targetSum] = False
-    return False
-
-# def canSum(targetSum, numbers, memo={}):
-#     """Return True if the targetSum can be computed using numbers.
-#     False if otherwise.
-#     targetSum       int
-#     numbers         list
-#     memo            dict{key:value} = {7:False}
-#     """
-#     # base case: result is saved in memo --> return memo
-#     if targetSum in memo:
-#         return memo[targetSum]
-    
-#     # base case: targetSum = 0 --> return True
-#     if targetSum == 0:  
-#         return True
-    
-#     # base case: targetSum < 0 --> return False
-#     if targetSum < 0:
-#         return False
-    
-#     # branching
-#     for num in numbers:
-#         remainder = targetSum - num
-#         # case: if remainder is reachable --> save result to memo, then return True
-#         if canSum(remainder, numbers, memo) == True:
-#             memo[targetSum] = True
-#             return True
         
-#     # no possible ways
-#     memo[targetSum] = False
-#     return False
+    # no possible ways
+    memo[targetSum] = False
+    level -= 1
+    # if reached the root (level == 0) --> delete the memo for further usage
+    if level == 0:
+        memo.clear()
+    return False
 
 if __name__ == '__main__':
     os.system('cls')
     print(canSum(7, [2, 3]))            # --> True
-    # print(canSum(7, [5, 3, 4, 7]))      # --> True
+    print(canSum(7, [5, 3, 4, 7]))      # --> True
     print(canSum(7, [2, 4]))            # --> False
-    # print(canSum(8, [2, 3, 5]))         # --> True
-    # print(canSum(300, [7, 14]))         # --> False
+    print(canSum(8, [2, 3, 5]))         # --> True
+    print(canSum(300, [7, 14]))         # --> False
