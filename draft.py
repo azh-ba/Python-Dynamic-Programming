7e92e274638cd94df31eec4dc84002768af16998
azh-ba <anhbanguyenquoc@gmail.com>
2024-03-25 12:40:27 +0700

    Update draft.py

--- draft.py
+++ draft.py
@@ -13,3 +13,15 @@ if __name__ == '__main__':
 
     print(n in test_dict)
     print(test_dict[n])
+    print()
+
+    original = [1, 2, 3]
+    temp1 = original[:]
+    temp2 = list(temp1) + [123]
+    temp3 = temp2
+
+    temp4 = original.append(4)
+    print(temp4)
+    print(temp1)
+    print(temp2)
+    print(temp3)
