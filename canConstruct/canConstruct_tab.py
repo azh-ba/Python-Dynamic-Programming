ec309df5167f4bb829e86f901e210e9566ca3a65
azh-ba <anhbanguyenquoc@gmail.com>
2024-03-29 23:00:48 +0700

    Update canConstruct_tab.py

--- canConstruct/canConstruct_tab.py
+++ canConstruct/canConstruct_tab.py
@@ -10,31 +10,35 @@ def canConstruct(target, wordBank):
     
     # assign default values for the table
     table[0] = True
+    prefix = ''
 
     # iterate through the table
     for i in range(len(table)):
         if i >= len(target):
             break
         else:
-            target_letter = target[i]
+            # get suffix
+            suffix = target.replace(prefix, '', 1)
             for word in wordBank:
                 # check word is a prefix of the target
-                if word[0] == target_letter:
-                    # check out of bound
-                    if (i + len(word)) <= len(target):
+                if suffix.startswith(word):
+                    # check out of bound & table[i] is True
+                    if ((i + len(word)) <= len(target)) and (table[i]):
                         table[i + len(word)] = True
+            # prepare prefix for the next suffix
+            prefix += target[i]
     
     return table[len(target)]
 
 if __name__ == '__main__':
     os.system('cls')
-    # print(canConstruct('abcdef', [
-    # 'ab', 
-    # 'abc', 
-    # 'cd', 
-    # 'def', 
-    # 'abcd'
-    # ]))         # --> True
+    print(canConstruct('abcdef', [
+    'ab', 
+    'abc', 
+    'cd', 
+    'def', 
+    'abcd'
+    ]))         # --> True
     print(canConstruct('skateboard', [
         'bo', 
         'rd', 
@@ -44,25 +48,25 @@ if __name__ == '__main__':
         'sk', 
         'boar'
     ]))         # --> False
-    # print(canConstruct('', [
-    #     'cat', 
-    #     'dog', 
-    #     'mouse'
-    # ]))         # --> True
-    # print(canConstruct('enterapotentpot', [
-    #     'a', 
-    #     'p', 
-    #     'ent', 
-    #     'enter', 
-    #     'ot', 
-    #     'o', 
-    #     't'
-    # ]))         # --> True
-    # print(canConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', [
-    #     'e',
-    #     'ee',
-    #     'eee',
-    #     'eeee',
-    #     'eeeee',
-    #     'eeeeee'
-    # ]))         # --> False
+    print(canConstruct('', [
+        'cat', 
+        'dog', 
+        'mouse'
+    ]))         # --> True
+    print(canConstruct('enterapotentpot', [
+        'a', 
+        'p', 
+        'ent', 
+        'enter', 
+        'ot', 
+        'o', 
+        't'
+    ]))         # --> True
+    print(canConstruct('eeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeef', [
+        'e',
+        'ee',
+        'eee',
+        'eeee',
+        'eeeee',
+        'eeeeee'
+    ]))         # --> False
